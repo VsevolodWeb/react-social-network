081aa53b04fdee173175ccabbb4c3f3a0798374b
Vsevolod Ivanov <fromdaytoday@icloud.com>
2019-10-22 21:01:12 +0300

    attempt to fix a redirect bug

--- src/hoc/withAuthRedirect.js
+++ src/hoc/withAuthRedirect.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Redirect} from 'react-router-dom';
+import Preloader from '../components/common/Preloader/Preloader';
 
 
 export const withAuthRedirect = Component => {
     class RedirectComponent extends React.Component {
+        constructor(props) {
+            super(props);
+            this.authFlag = true;
+        }
+
         render() {
-            if(!this.props.isAuth) return <Redirect to="/login" />;
+            if(!this.props.isAuth) {
+                this.authFlag = !this.authFlag;
+                return this.authFlag ? <Redirect to="/login" /> : <Preloader />;
+            }
             return <Component {...this.props} />
         }
     }
