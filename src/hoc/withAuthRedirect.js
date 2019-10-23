b20feb439dd0647b4cacd2e315e2334e09e1bbda
Vsevolod Ivanov <fromdaytoday@icloud.com>
2019-10-23 22:54:27 +0300

    old redirect returned

--- src/hoc/withAuthRedirect.js
+++ src/hoc/withAuthRedirect.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Redirect} from 'react-router-dom';
-import Preloader from '../components/common/Preloader/Preloader';
 
 
 export const withAuthRedirect = Component => {
     class RedirectComponent extends React.Component {
-        constructor(props) {
-            super(props);
-            this.authFlag = true;
-        }
-
         render() {
-            if(!this.props.isAuth) {
-                this.authFlag = !this.authFlag;
-                return this.authFlag ? <Redirect to="/login" /> : <Preloader />;
-            }
+            if(!this.props.isAuth) return <Redirect to="/login" />;
             return <Component {...this.props} />
         }
     }
