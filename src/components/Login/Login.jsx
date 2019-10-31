f20dc15dbb2b8a53ba9632d276b817f8ac5d5a4b
Vsevolod Ivanov <fromdaytoday@icloud.com>
2019-10-31 21:39:04 +0300

    Login form beginning

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,7 +6,22 @@ const Login = () => {
         <>
             <h1 className="title">Login</h1>
             <div className={s.login}>
-                login text
+                <div className="form">
+                    <div className="formGroup">
+                        <input type="text" placeholder="Login" className="input" />
+                    </div>
+                    <div className="formGroup">
+                        <input type="password" placeholder="Password" className="input" />
+                    </div>
+                    <div className="formGroup">
+                        <label htmlFor="remember-me">
+                            <input type="checkbox" id="remember-me" /> Remember me
+                        </label>
+                    </div>
+                    <div className="formGroup">
+                        <button className="button">Login</button>
+                    </div>
+                </div>
             </div>
         </>
     )
