909c177aeaeb3bb69b4eaaf69ba9adad890af2e5
Vsevolod Ivanov <fromdaytoday@icloud.com>
2019-11-16 21:51:20 +0300

    useEffect in status profile

--- src/components/Profile/Info/Status/StatusWithHooks.jsx
+++ src/components/Profile/Info/Status/StatusWithHooks.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import s from './Status.module.css'
 
 const StatusWithHooks = props => {
 
     const [editMode, setEditMode] = useState(false);
     const [status, setStatus] = useState(props.status);
+
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status]);
 
     const toggleEditMode = () => {
         setEditMode(!editMode);
