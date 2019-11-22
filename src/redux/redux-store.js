73c4e8a7aeb7f4be9d23fe5a8dbcd5aa0ecf3a26
Vsevolod <fromdaytoday@icloud.com>
2019-11-22 18:59:31 +0300

    redux in browser

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import thunkMiddleware from 'redux-thunk'
 
 import {reducer as formReducer} from 'redux-form';
@@ -39,4 +39,10 @@ const reducers = combineReducers({
     
 });
 
-export default createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+));
+
+//export default createStore(reducers, applyMiddleware(thunkMiddleware));
+export default store;
