d93297b65cc00e159be79c952b09b4715825891a
Vsevolod <fromdaytoday@icloud.com>
2019-11-28 18:42:54 +0300

    doubleclick test

--- src/components/Profile/Info/Status/StatusWithHooks.test.js
+++ src/components/Profile/Info/Status/StatusWithHooks.test.js
@@ -1,16 +1,25 @@
 import React from "react";
-import { create } from "react-test-renderer"
+import { create, act } from "react-test-renderer"
 import StatusWithHooks from "./StatusWithHooks";
 
 
-
 describe("Status component", () => {
-    const component = create(<StatusWithHooks status="test status"/>);
+    const componentStatusText = "test status";
+    const component = create(<StatusWithHooks editingAbility="true" status={componentStatusText}/>);
+
     it("status should be created successfully", () => {
         expect(component.toJSON()).toMatchSnapshot();
-    })
+    });
 
-    it("test", () => {
-        expect(component.toJSON()).toMatchSnapshot();
-    })
+    it("status double click should be work", () => {
+        let status = component.root.findByProps({className: "status status_editingAbility"});
+
+        act(() => {
+            status.props.onDoubleClick();
+        });
+
+        let input = component.root.findByType("input");
+
+        expect(input.props.value).toBe(componentStatusText)
+    });
 });
