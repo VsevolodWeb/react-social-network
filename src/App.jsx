52bb6076d2cb02c6c5913db6386e427ded8b5288
Vsevolod Ivanov <fromdaytoday@icloud.com>
2019-12-02 10:56:04 +0200

    pagination refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 
 import HeaderContainer from './components/Header/HeaderContainer';
 import SidebarContainer from './components/Sidebar/SidebarContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
 import MessagesContainer from './components/Messages/MessagesContainer';
-import UsersContainer from './components/Users/UsersContainer';
 import Login from './components/Login/Login';
 import Preloader from './components/common/Preloader/Preloader';
+
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class App extends React.Component {
   componentDidMount() {
@@ -27,9 +28,13 @@ class App extends React.Component {
           <div className="container mainGrid">
             <SidebarContainer />
             <div className="content">
-              <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
+              <Route path="/profile/:userId?" render={() => {
+                return <Suspense fallback={<Preloader/>}><ProfileContainer /></Suspense>
+              }}/>
               <Route path="/messages" render={() => <MessagesContainer />} />
-              <Route path="/users" render={() => <UsersContainer />} />
+              <Route path="/users" render={() => {
+                return <Suspense fallback={<Preloader/>}><UsersContainer /></Suspense>
+              }} />
               <Route path="/login" render={() => <Login />} />
             </div>
           </div>
--- src/components/common/Pagination/Pagination.jsx
+++ src/components/common/Pagination/Pagination.jsx
@@ -4,6 +4,7 @@ import s from './Pagination.module.css'
 const Pagination = props => {
     let pagesCount =  Math.ceil(props.totalCount / props.pageSize);
     let pagesCountArray = [];
+    let currentPage = props.currentPage
     const portion = 10;
     const halfPortion = portion / 2;
 
@@ -14,36 +15,34 @@ const Pagination = props => {
         return null;
     }
 
-    if(props.currentPage > halfPortion && props.currentPage < pagesCount - halfPortion) {
-        startCount = props.currentPage - halfPortion;
-        endCount = props.currentPage + halfPortion;
-    } else if(props.currentPage >= pagesCount - halfPortion) {
+    if(currentPage > halfPortion && currentPage < pagesCount - halfPortion) {
+        startCount = currentPage - halfPortion;
+        endCount = currentPage + halfPortion;
+    } else if(currentPage >= pagesCount - halfPortion) {
         startCount = pagesCount - portion;
         endCount = pagesCount;
     }
 
     for(let i = startCount; i <= endCount; i++) {
         pagesCountArray.push(
-            <li className={s.pagination__item + (props.currentPage === i ? (" " + s.pagination__item_active) : "")} key={i}>
+            <li className={s.pagination__item + (currentPage === i ? (" " + s.pagination__item_active) : "")} key={i}>
                 <button onClick={() => props.setCurrentPage(i)} className={s.pagination__button}>{i}</button>
             </li>);
     }
 
     const changePage = direction => {
-        let currentPageId = props.currentPage;
-
         switch (direction) {
             case 'prev':
-                currentPageId -= 1;
+                currentPage -= 1;
                 break;
             case 'next':
-                currentPageId += 1;
+                currentPage += 1;
                 break;
             default:
                 return;
         }
 
-        props.setCurrentPage(currentPageId);
+        props.setCurrentPage(currentPage);
     }
 
     return (
