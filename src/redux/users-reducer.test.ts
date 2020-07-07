cc6bb37061ee9b3735a4256f338832623d385d44
vsevolod <fromdaytoday@icloud.com>
2020-07-07 19:01:15 +0300

    users-reducer.test.ts added

---
+++ src/redux/users-reducer.test.ts
@@ -0,0 +1,14 @@
+import {InitialStateType} from "./users-reducer";
+
+test("", () => {
+    const state: InitialStateType = {
+        list: [],
+        pageSize: 20,
+        totalUsersCount: 0,
+        currentPage: 1,
+        isFetching: false,
+        isFollowing: []
+    }
+
+    //expect()
+});
